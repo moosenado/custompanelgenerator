73f4e00b846852eace1c33571e741462a9dfe6e5
moosenado <egrabish@gmail.com>
2017-04-08 11:01:37 -0400

    beginnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+
+import tflogo from './../public/imgs/testforce_logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
+      <div className="app">
+      
+      	<header>
+        	<img src={tflogo} alt="company logo" role="presentation"/>
+        </header>
+
+        <main className="app-container">
+        </main>
+
+        <footer>
+        </footer>
+
       </div>
     );
   }
