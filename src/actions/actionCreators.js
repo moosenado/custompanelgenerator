5197f9fd7e2ff335035b20a67a8461b92f9be24d
moosenado <egrabish@gmail.com>
2017-04-23 10:06:23 -0400

    ajax comment

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,5 +1,6 @@
 import { initConnectors } from './actions';
 
+//ADD YOUR AJAX REQUEST IN HERE TO GET INIT DATA
 export function initialStateConnection() {
     return (dispatch) => {
         dispatch(initConnectors({
